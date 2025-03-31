7c3c62a9533d4ad08108465da9195e09870b8f95
Cullen999 <arthur.danilov99@gmail.com>
2025-03-31 13:17:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,207 +1,187 @@
+const handle = document.querySelectorAll('.fader-handle');
+const speedDisplay = document.querySelectorAll('.speed-display');
+const speedVinyl = document.querySelectorAll('.vinyl');
+const knob = document.querySelectorAll('.knob');
+const lights = Array.from(document.querySelectorAll('.led-light')).reverse();
+const audio = document.getElementById('audio');
+
+// fader
+
+handle.forEach(handl => {
+    let isDragging = false;
+    let clientY;
+    let startTop = parseInt(handl.style.top);
+
+   
+
+    handl.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        clientY = e.clientY;
+    }) 
+
+    document.addEventListener('mousemove', (e) => {
+
+        // fader move
+
+        if(!isDragging) return;
+        
+        let distTrav = e.clientY - clientY ;
+        clientY = e.clientY;
+
+        startTop = Math.max(0, Math.min(140, startTop + distTrav));
+
+        handl.style.top = `${startTop}px`;
+
+        
+        // speed display
+
+        let speedDis = 3 - (startTop * 2 / 140);
+
+        if(handl.dataset.control == 'speedL') speedDisplay[0].textContent = `Speed: ${speedDis.toFixed(1)}x`;
+        if(handl.dataset.control == 'speedR') speedDisplay[1].textContent = `Speed: ${speedDis.toFixed(1)}x`;
+
+        // speed vinyl
+
+        let speedVin = 1 + (startTop * 2 / 140)
+
+        if(handl.dataset.control == 'speedL') speedVinyl[0].style.animation = `spin ${speedVin.toFixed(1)}s linear infinite`;
+        if(handl.dataset.control == 'speedR') speedVinyl[1].style.animation = `spin ${speedVin.toFixed(1)}s linear infinite`
+       
+        
+    })
 
 
-let audio;
-async function loadAudio() {
-    const response = await fetch("https://cullen999.github.io/equalizer/UltraCyberBass.mp3");
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+});
 
-    audio = document.getElementById("cyberaudio");
-    audio.src = url;
-    audio.volume = 0;
+
+
+// knob equalizer
+
+
+const intervalMap = new Map(); // 
+
+
+function equalizer(volumeMeter, index, light) {
+    let diff = volumeMeter - index;
+
+    if (diff < 0 && diff >= -30) {
+            console.log(volumeMeter)
+            if (intervalMap.size >= 3) {
+                    
+                intervalMap.forEach(id => clearInterval(id))
+                intervalMap.clear();
+                
+            }
+
+            if(volumeMeter >= 10 && volumeMeter < 80) {
+                let id = setInterval(() => {
+                    light.style.opacity = light.style.opacity === "0.1" ? "0.9" : "0.1"; 
+                }, diff >= -10 ? 400 : diff >= -20 ? 700 : 900);
+                
+                intervalMap.set(index, id); 
+            } 
+            
+            
+    }
 }
 
-loadAudio();
+// knob 
 
-let animationDuration = 2.0;
+knob.forEach(knb => {
 
-
-
-document.querySelectorAll('.fader-handle').forEach(handle => {
     let isDragging = false;
-    let startTop, startY;
-    handle.addEventListener('mousedown', (e) => {
-        audio.play();
+    let startAngle = Math.atan2(0, 1) ;
+    const rect = knb.getBoundingClientRect();
+    const centerY = rect.top + rect.height / 2;
+    const centerX = rect.left + rect.width / 2;
+    let acc = 0;
+    
+    knb.addEventListener('mousedown', (e) => {
+        // knob down
+        
         isDragging = true;
-        startY = e.clientY;
-        startTop = parseInt(handle.style.top);
-  
-    })
+
+        let radian = Math.atan2(-(centerX - e.clientX), centerY - e.clientY);
+        
+        let distance = radian - startAngle;
+        let gradus = distance * (180 / Math.PI);
+        
+        acc += gradus;
+        startAngle = radian;
+        
+        if(acc >= 0 && acc <= 153) {
+            knb.style.transform = `rotate(${acc}deg)`;
+
+            // volume meter
+
+            if(knb.dataset.control == 'volume') {
+                const volumeMeter = acc * 10 / 15;
+                let audioVolume = volumeMeter / 100;
+                audioVolume > 1.0 ? audioVolume = 1.0 : audioVolume;
+                audio.volume = audioVolume;
+                audio.play();
+                
+                lights.forEach((light, i) => {
+                    let index = (i + 1) * 10;
+                    
+                    volumeMeter - index >= 0 ? light.style.opacity = '0.9' : light.style.opacity = '0.1';
+                    
+                    equalizer(volumeMeter, index, light);
+                     
+                })
+        
+            }
+        }
+
+    });
+
+        
 
     document.addEventListener('mousemove', (e) => {
         if(!isDragging) return;
-        const deltaY = e.clientY - startY;
-        let newTop = startTop + deltaY;
 
-        newTop = Math.max(0, Math.min(140, newTop));
-
-        handle.style.top = newTop + 'px';
-
-       let spiner = document.querySelectorAll('.vinyl');
-       let speedDisplay = document.querySelectorAll('.speed-display');
-       let speed = false;
-
-       if(newTop % 7 == 0) {
-        speed = 3 - (newTop / 140) * 2;
-       }  
-       
+        // knob move
+        let radian = Math.atan2(-(centerX - e.clientX), centerY - e.clientY);
         
-       if(speed) {
-        animationDuration = (4 - speed);
+        let distance = radian - startAngle;
+        let gradus = distance * (180 / Math.PI);
         
-       if(handle.dataset.control == "speedL" || handle.dataset.control == "speedR") audio.playbackRate = speed; 
-       handle.dataset.control == "speedL" ? spiner[0].style.animation = `spin ${animationDuration}s linear infinite` : '';
-       handle.dataset.control == "speedL" ? speedDisplay[0].textContent = `Speed: ${speed.toFixed(1)}x` : '';
-       handle.dataset.control == "speedR" ? spiner[1].style.animation = `spin ${animationDuration}s linear infinite` : '';
-       handle.dataset.control == "speedR" ? speedDisplay[1].textContent = `Speed: ${speed.toFixed(1)}x` : '';
-       
-       }
-    })
+        acc += gradus;
+        startAngle = radian;
+        
+        if(acc >= 0 && acc <= 153) {
+            knb.style.transform = `rotate(${acc}deg)`;
 
-    document.addEventListener('mouseup', (e) => {
+             // volume meter
+
+            if(knb.dataset.control == 'volume') {
+                const volumeMeter = acc * 10 / 15;
+                
+                let audioVolume = volumeMeter / 100;
+                audioVolume > 1.0 ? audioVolume = 1.0 : audioVolume;
+                audio.volume = audioVolume;
+                audio.play();
+
+                lights.forEach((light, i) => {
+                    let index = (i + 1) * 10;
+                    
+                    volumeMeter - index >= 0 ? light.style.opacity = '0.9' : light.style.opacity = '0.1';
+    
+                    equalizer(volumeMeter, index, light)
+                });
+        
+            }
+        }
+        
+    });
+
+    document.addEventListener('mouseup', () => {
         isDragging = false;
     })
 });
 
 
-
-document.querySelectorAll('.vinyl').forEach(vinyl => {
-    let isDragging = false
-   
-    vinyl.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        if(vinyl.id == "leftVinyl") vinyl.style.animation = `spin 0s linear infinite`;
-        if(vinyl.id == "rightVinyl") vinyl.style.animation = `spin 0s linear infinite`;
-
-        audio.pause();
-       
-    })
-
-    document.addEventListener('mouseup', (e) => {
-        if(!isDragging) return;
-        vinyl.style.animation = `spin ${animationDuration}s linear infinite`;
-        audio.play();
-    })
-})
-
-const leds = Array.from(document.querySelectorAll(".led-light")).reverse();
-let audioContext = null;
-
-function AudioVisualizer() {
-        if(audioContext) return;
-    
-        audioContext = new window.AudioContext();
-    
-        let src = audioContext.createMediaElementSource(audio);
-        let analyser = audioContext.createAnalyser();
-        analyser.fftSize = 32;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        src.connect(analyser);
-        analyser.connect(audioContext.destination);
-    
-       
-    
-
-    function up() {
-        if(!analyser) return;
-        analyser.getByteFrequencyData(dataArray);
-
-        let averageVolume = dataArray.reduce((acc, freq) => acc + freq, 0) / bufferLength;
-        const lvl = Math.round((averageVolume / 255) * leds.length);
-
-        leds.forEach((light, index) => {
-            light.style.opacity = index < lvl ? 1 : 0.2;
-        });
-
-        requestAnimationFrame(up);
-    }
-
-    audio.onplay = () => {
-        if (audioContext.state === "suspended") {
-            audioContext.resume();
-        }
-            up();
-        
-        
-    };
-}
-
-
-
-
-document.querySelectorAll('.knob').forEach(knob => {
-    
-   let startAngle =  Math.atan2(0, 1);
-   let rotation = 0;
-   let isDragging = false;
-
-
-   knob.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        const rect = knob.getBoundingClientRect();
-        
-		const centerX = rect.left + rect.width / 2;
-		const centerY = rect.top + rect.height / 2;
-		const currentAngle = Math.atan2( e.clientX - centerX ,-(e.clientY - centerY));
-        
-        const angleDiff = currentAngle - startAngle;
-        
-		rotation += angleDiff * (180 / Math.PI);
-		rotation = Math.min(150, Math.max(0, rotation));
-        
-        knob.style.transform = `rotate(${rotation}deg)`;
-        startAngle = currentAngle;
-        let vol = rotation / 150;
-		
-        if(knob.dataset.control == 'volume') {
-            audio.volume = vol;
-            audio.play();
-            AudioVisualizer();
-        }
-
-        
-        
-        
-
-   })
-
-   document.addEventListener('mousemove', (e) => {
-    if(!isDragging) return;
-    
-    const rect = knob.getBoundingClientRect();
-        
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-    const currentAngle = Math.atan2( e.clientX - centerX ,-(e.clientY - centerY));
-    
-    const angleDiff = currentAngle - startAngle;
-    
-    rotation += angleDiff * (180 / Math.PI);
-    console.log(angleDiff * (180 / Math.PI));
-    rotation = Math.min(150, Math.max(0, rotation));
-    
-    
-    
-    knob.style.transform = `rotate(${rotation}deg)`;
-    startAngle = currentAngle;
-    let vol = rotation / 150;
-
-    if(knob.dataset.control == 'volume') {
-        audio.volume = vol;
-        audio.play();
-        AudioVisualizer();
-    }
-
-    
-
-        
-    
-   })
-
-   document.addEventListener('mouseup', () => {
-    isDragging = false;
-   })
-
-});
 
